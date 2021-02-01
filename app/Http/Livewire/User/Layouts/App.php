6643a9c193f035dccf1b9838529d090779d51480
misusonu18 <misusonu@gmail.com>
2021-02-01 09:41:57 +0530

    laravel and livewire Install

---
+++ app/Http/Livewire/User/Layouts/App.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\User\Layouts;
+
+use Livewire\Component;
+
+class App extends Component
+{
+    public function render()
+    {
+        return view('livewire.user.layouts.app');
+    }
+}
